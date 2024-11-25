79be390100e122eae3517281903c9f0952a559a5
Nischitha1309 <150625669+Nischitha1309@users.noreply.github.com>
2024-11-25 16:07:25 -0500

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 
 # Title of the app
 st.title("E-commerce and Retail Sales Data Analysis")
